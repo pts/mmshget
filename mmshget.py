58550d9b3782b8b1d5a28bf554c3f69da9ea50a2
Peter Szabo <pts@fazekas.hu>
2012-03-31 10:15:26 +0000

    updated mmshget

--- mmshget.py
+++ mmshget.py
@@ -1,6 +1,124 @@
 #! /usr/bin/python2.4
+# by pts@fazekas.hu at Sat Mar 24 17:32:25 CET 2012
 
+import os
+import re
+import socket
 import struct
+import sys
+
+# --- Proxy
+
+HTTP_PROXY_RE = re.compile(r'(?:\w+://)?([^+%@?#:/]+)(?::(\d+))?(?:/|\Z)')
+
+def GetProxyForHost(host):
+  """Returns None or (proxy_host, proxy_port)."""
+  http_proxy = os.getenv('http_proxy', '')
+  if not http_proxy:
+    return None
+  for item in os.getenv('no_proxy', '').split(','):
+    if item:
+      if item.starswith('.'):
+        if host.endswith(item):
+          return None
+      else:
+        if host == item:
+          return None
+  match = HTTP_PROXY_RE.match(http_proxy)
+  assert match, 'Bad http_proxy=%r' % http_proxy
+  proxy_host = match.group(1)
+  if match.group(1) is None:
+    proxy_port = 80
+  else:
+    proxy_port = int(match.group(2))
+  assert 1 <= proxy_port <= 65535, 'Bad proxy_port=%d, http_proxy=%r' % (
+      proxy_port, http_proxy)
+  return (proxy_host, proxy_port)
+
+URL_RE = re.compile(r'(?:http|mmsh)://([^+%@?#:/]+)(?::(\d+))?(/[^\s#]*)?\Z')
+"""This is the URL subset syntax we support."""
+
+RESPONSE_LINE1_RE = re.compile(
+    r'HTTP/(1[.][01]) +(\d{3}) +(\S[^\r\n]*)\r?\n\Z')
+RESPONSE_HEADER_RE = re.compile(r'([A-Za-z][-\w]*): ?([^\r\n]*)\Z')
+
+def DoHttpRequest(url, request_headers=(), timeout=10):
+  """Send a HTTP GET request.
+  
+  Args:
+    url: String containing an http:// or mmsh:// URL.
+    request_headers: Sequence of strings containing request headers to send.
+    timeout: Timeout for each socket operation, in seconds.
+  Returns:
+    Returns a file-like object for reading the response body.
+  """
+  # Not using `import urllib', because that doesn't support proxies.
+  match = URL_RE.match(url)
+  assert match, 'Bad url=%r' % (url,)
+  host = match.group(1)
+  if match.group(2) is None:
+    port = 80
+  else:
+    port = int(match.group(2))
+  assert 1 <= port <= 65535, 'Bad request port=%d, url=%r' % (port, url)
+  path = match.group(3) or '/'
+  if port == 80:
+    hostport = host
+  else:
+    hostport = '%s:%s' % (host, port)
+
+  proxy_address = GetProxyForHost(host)
+  if proxy_address:
+    connect_address = proxy_address
+    req = ['GET http://%s%s HTTP/1.0\r\nHost: %s\r\n' %
+           (hostport, path, hostport)]
+  else:
+    connect_address = (host, port)
+    req = ['GET %s HTTP/1.0\r\nHost: %s\r\n' % (path, hostport)]
+  for header in request_headers:
+    header = header.rstrip('\r\n')
+    if header:
+      assert ':' in header, 'Missing colon in request_header=%r' % (header,)
+      assert '\n' not in header, (
+          'Unexpected newline in request_header=%r' % (header,))
+      assert '\r' not in header, (
+          'Unexpected CR in request_header=%r' % (header,))
+      req.append(header + '\r\n')
+  req.append('\r\n')
+  req = ''.join(req)
+
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+  sock.settimeout(timeout)
+  sock.connect(connect_address)
+  sock.sendall(req)
+  f = sock.makefile()  # For reading.
+  del sock
+
+  line1 = f.readline()
+  match = RESPONSE_LINE1_RE.match(line1)
+  assert match, 'Bad HTTP response line1=%r' % line1
+  response_protocol = match.group(1)
+  response_status = int(match.group(2))
+  response_message = match.group(2)
+  # TODO(pts): Add support for redirects, e.g. from http://www.example.org/
+  assert response_status == 200, (
+      'HTTP server returned error status=%d message=%r' %
+      (response_status, response_message))
+  del line1, response_protocol, response_status, response_message
+
+  while True:
+    line = f.readline()
+    assert line, 'Unexpected EOF in HTTP response headers.'
+    line = line.rstrip('\r\n')
+    if not line:
+      break
+    match = RESPONSE_HEADER_RE.match(line)
+    assert match, 'Bad HTTP response line=%r' % line
+
+  return f
+
+
+# --- mmsh:// downloader
 
 CHUNK_TYPE_RESET      = 0x4324
 CHUNK_TYPE_DATA       = 0x4424
@@ -18,6 +136,8 @@ NAME_FROM_TYPE = {
   CHUNK_TYPE_END        : 'END',
   CHUNK_TYPE_ASF_HEADER : 'ASF_HEADER',
 }
+
+ASF_HEADER_MAX_SIZE = 16384
 
 # base ASF objects
 GUID_HEADER = '3026b2758e66cf11a6d900aa0062ce6c'
@@ -79,83 +199,150 @@ GUID_ASF_20_HEADER = 'd129e2d6da35d111903400a0c90349be'
 # ce75f87b8d46d1118d82006097c9a2b2 GUID_STREAM_BITRATE_PROPERTIES
 # 3626b2758e66cf11a6d900aa0062ce6c GUID_DATA
 
-f = open('g2.asf')
-outf = open('g2.asf.dump', 'w')
-expected_seq = 0
-processed_asf_header = False
-pos = 0
-asf_head = ''
-packet_size = 0
-while True:  # It's an error not to have the END chunk.
-  chunk_pos = pos
-  print '@%d' % pos
-  chunk_head = f.read(4)
-  pos += len(chunk_head)
-  assert len(chunk_head) == 4
-  chunk_type, chunk_size = struct.unpack('<HH', chunk_head)
-  ext_header_size = EXT_HEADER_SIZES.get(chunk_type)
-  assert ext_header_size is not None, 'Unknown chunk type=0x%x' % chunk_type
-  ext_head = f.read(ext_header_size)
-  pos += len(ext_head)
-  chunk_size -= ext_header_size
-  print '@%d headext_size=%d type=%s size=%d' % (
-      chunk_pos, 4 + ext_header_size, NAME_FROM_TYPE[chunk_type], chunk_size)
+def ParseAsfHeader(asf_head):
+  asf_info = {}
+  assert asf_head, 'Missing ASF header.'
+  i = 30
+  packet_size = 0
+  while i + 24 <= len(asf_head):
+    guid, size = struct.unpack('<16sQ', asf_head[i : i + 24])
+    assert size >= 24
+    if size > 65535:
+      # Example: size=0xaba1b2 remaining=0x32
+      # assert 0, 'size=0x%x remaining=0x%x' % (size, len(asf_head) - i)
+      i = len(asf_head)
+      break
+    assert i + size <= len(asf_head), (i + size, size, len(asf_head))
+    guid_hex = guid.encode('hex')
+    # TODO(pts): Get file size for progress bar etc.
+    if guid_hex == GUID_ASF_FILE_PROPERTIES:
+      assert size >= 100
+      packet_size, = struct.unpack('<L', asf_head[i + 92 : i + 96])
+      assert packet_size > 0
+      assert packet_size <= 65536, 'Too large packet_size=%d' % packet_size
+    i += size
+  assert i == len(asf_head)
+  assert packet_size > 0, 'Could not find packet_size in ASF header.'
+  asf_info['packet_size'] = int(packet_size)
+  return asf_info
 
-  if chunk_type == CHUNK_TYPE_DATA:
-    seq, = struct.unpack('<L', ext_head[:4])
-    assert expected_seq == seq, 'Bad seq: expected=%d got=%d' % (
-        expected_seq, seq)
-    expected_seq += 1
-  elif chunk_type == CHUNK_TYPE_END:
-    seq, = struct.unpack('<L', ext_head[:4])
-    assert seq in (0, 1), 'Unexpected seq=%d for END' % seq
-    if seq == 1:
-      raise NotImplementedError('Subsequent HTTP request not supported.')
-    break
-  elif chunk_type == CHUNK_TYPE_RESET:
+
+def DoFirstAsfRequest(url):
+  # request-context below is the 
+  headers = (
+      'Accept: */*',
+      'User-Agent: NSPlayer/4.1.0.3856',
+      'Pragma: no-cache,rate=1.000000,stream-time=0,stream-offset=0:0,request-context=1,max-duration=0',
+      'Pragma: xClientGUID={c77e7400-738a-11d2-9add-0020af0a3278}',
+  )
+  # TODO(pts): Open the other source after timeout.
+  f = DoHttpRequest(url, headers)
+
+  try:
+    # Read and parse the ASF header.
     asf_head = ''
-    processed_asf_header = False
-    assert chunk_size, 'Unexpected chunk_size=%d' % chunk_size
-  elif chunk_type == CHUNK_TYPE_ASF_HEADER:
-    assert not processed_asf_header, 'Unexpected ASF_HEADER.'
-    assert len(asf_head) + chunk_size <= 16384, 'ASF header too long.'
-  else:
-    assert 0, 'Unexpected chunk type=0x%x' % chunk_type
-
-  if chunk_type != CHUNK_TYPE_ASF_HEADER and not processed_asf_header:
-    # All chunks of the ASF header has been read, interpret asf_head.
-    assert asf_head, 'Missing ASF header.'
-    i = 30
-    packet_size = 0
-    while i + 24 <= len(asf_head):
-      guid, size = struct.unpack('<16sQ', asf_head[i : i + 24])
-      assert size >= 24
-      if size > 65535:
+    while True:
+      chunk_head = f.read(4)
+      if asf_head and not chunk_head:
         break
-      assert i + size <= len(asf_head), (i + size, size, len(asf_head))
-      guid_hex = guid.encode('hex')
-      # TODO(pts): Get file size for progress bar etc.
-      if guid_hex == GUID_ASF_FILE_PROPERTIES:
-        assert size >= 100
-        packet_size, = struct.unpack('<L', asf_head[i + 92 : i + 96])
-        assert packet_size > 0
-        assert packet_size <= 65536, 'Too large packet_size=%d' % packet_size
-      i += size
-    #assert i == len(asf_head)
-    asf_head = ''  # Save memory.
-    processed_asf_header = True
+      assert len(chunk_head) == 4, 'Unexpected EOF in chunk_head=%r' % (
+          chunk_head)
+      chunk_type, chunk_size = struct.unpack('<HH', chunk_head)
+      assert chunk_type == CHUNK_TYPE_ASF_HEADER, (
+          'Expected chunk ASF header, got chunk_type=0x%x' % chunk_type)
+      ext_header_size = EXT_HEADER_SIZES[chunk_type]
+      ext_head = f.read(ext_header_size)
+      assert len(ext_head) == ext_header_size
+      chunk_size -= ext_header_size    
+      assert len(asf_head) + chunk_size <= ASF_HEADER_MAX_SIZE, (
+          'ASF header too long.')
+      chunk_data = f.read(chunk_size)
+      assert len(chunk_data) == chunk_size
+      asf_head += chunk_data
+      del chunk_data
 
-  # CHUNK_TYPE_DATA has mms seq in ext_head[5]
-  # CHUNK_TYPE_DATA and CHUNK_TYPE_ASF_HEADER have len2 in ext_head[6 : 8]
-  chunk_data = f.read(chunk_size)
-  pos += len(chunk_data)
-  # print 'DUMP size=%d %r' % (len(chunk_data), chunk_data)
-  outf.write(chunk_data)
-  if chunk_type == CHUNK_TYPE_DATA:
-    assert chunk_size <= packet_size, 'Bad chunk_size=%d, packet_size=%d' % (
-        chunk_size, packet_size)
-    if packet_size > chunk_size:
-      outf.write('\0' * (packet_size - chunk_size))  # Padding.
-  elif chunk_type == CHUNK_TYPE_ASF_HEADER:
-    asf_head += chunk_data
-  assert len(chunk_data) == chunk_size
+    return ParseAsfHeader(asf_head)
+  finally:
+    f.close()
+
+def DownloadAsfStream(f, outf):
+  expected_seq = 0
+  processed_asf_header = False
+  pos = 0
+  asf_head = ''
+  packet_size = 0
+  while True:  # It's an error not to have the END chunk.
+    chunk_pos = pos
+    print '@%d' % pos
+    chunk_head = f.read(4)
+    pos += len(chunk_head)
+    assert len(chunk_head) == 4, 'Unexpected EOF in chunk_head=%r' % chunk_head
+    chunk_type, chunk_size = struct.unpack('<HH', chunk_head)
+    ext_header_size = EXT_HEADER_SIZES.get(chunk_type)
+    assert ext_header_size is not None, 'Unknown chunk type=0x%x' % chunk_type
+    ext_head = f.read(ext_header_size)
+    pos += len(ext_head)
+    chunk_size -= ext_header_size
+    assert len(ext_head) == ext_header_size
+    print '@%d headext_size=%d type=%s size=%d' % (
+        chunk_pos, 4 + ext_header_size, NAME_FROM_TYPE[chunk_type], chunk_size)
+
+    if chunk_type == CHUNK_TYPE_DATA:
+      seq, = struct.unpack('<L', ext_head[:4])
+      assert expected_seq == seq, 'Bad seq: expected=%d got=%d' % (
+          expected_seq, seq)
+      expected_seq += 1
+    elif chunk_type == CHUNK_TYPE_END:
+      seq, = struct.unpack('<L', ext_head[:4])
+      assert seq in (0, 1), 'Unexpected seq=%d for END' % seq
+      if seq == 1:
+        raise NotImplementedError('Subsequent HTTP request not supported.')
+      break
+    elif chunk_type == CHUNK_TYPE_RESET:
+      asf_head = ''
+      processed_asf_header = False
+      assert chunk_size, 'Unexpected chunk_size=%d' % chunk_size
+    elif chunk_type == CHUNK_TYPE_ASF_HEADER:
+      assert not processed_asf_header, 'Unexpected ASF_HEADER.'
+      assert len(asf_head) + chunk_size <= ASF_HEADER_MAX_SIZE, (
+          'ASF header too long.')
+    else:
+      assert 0, 'Unexpected chunk type=0x%x' % chunk_type
+
+    if chunk_type != CHUNK_TYPE_ASF_HEADER and not processed_asf_header:
+      # All chunks of the ASF header has been read, interpret asf_head.
+      asf_info = ParseAsfHeader(asf_head)
+      packet_size = asf_info['packet_size']
+      asf_head = ''  # Save memory.
+      processed_asf_header = True
+
+    # CHUNK_TYPE_DATA has mms seq in ext_head[5]
+    # CHUNK_TYPE_DATA and CHUNK_TYPE_ASF_HEADER have len2 in ext_head[6 : 8]
+    chunk_data = f.read(chunk_size)
+    pos += len(chunk_data)
+    assert len(chunk_data) == chunk_size
+    # print 'DUMP size=%d %r' % (len(chunk_data), chunk_data)
+    outf.write(chunk_data)
+    if chunk_type == CHUNK_TYPE_DATA:
+      assert chunk_size <= packet_size, 'Bad chunk_size=%d, packet_size=%d' % (
+          chunk_size, packet_size)
+      if packet_size > chunk_size:
+        outf.write('\0' * (packet_size - chunk_size))  # Padding.
+    elif chunk_type == CHUNK_TYPE_ASF_HEADER:
+      asf_head += chunk_data
+    assert len(chunk_data) == chunk_size
+
+
+
+if False:
+  f = open('g2.asf')
+  outf = open('g2.asf.dump', 'w')
+  DownloadAsfStream(f, outf)
+
+
+if __name__ == '__main__':
+  #f = DoHttpRequest('http://www.iana.org/domains/example/')
+
+  url = 'http://streamer3.carnation.hu/mtvod2/hirado/2012/03/14/idojaras_20120314_122.wmv?MSWMExt=.asf'
+  asf_info = DoFirstAsfRequest(url)
+  assert 0, asf_info
